036352826d810b6e85f41c3fe8c32c8e948103df
daimler.yangl14 <liyang.yl.li@daimler.com>
2020-04-24 10:39:09 +0800

    Add 4 LiDAR topics and GNSS ins state.

--- record.sh
+++ record.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 source /apollo/scripts/apollo_base.sh
-rosbag record -O multi_lidar_gnss.bag /apollo/sensor/velodyne32/PointCloud2_left_front  /apollo/sensor/gnss/odometry
-mkdir -p /apollo/data/bag/calibration/
-mv multi_lidar_gnss.bag /apollo/data/bag/calibration/
+rosbag record -O multi_lidar_gnss.bag /apollo/sensor/velodyne32/PointCloud2_left_front  /apollo/sensor/gnss/odometry /apollo/sensor/velodyne32/PointCloud2_right_front /apollo/sensor/velodyne32/PointCloud2_left_rear /apollo/sensor/velodyne32/PointCloud2_right_rear /apollo/sensor/gnss/best_pose /apollo/sensor/gnss/ins_stat
